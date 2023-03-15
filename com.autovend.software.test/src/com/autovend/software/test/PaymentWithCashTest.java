8088ac1cee91cda577d13784bb7c663a38d17a24
aarontigs <aaron.tigley@gmail.com>
2023-03-15 01:00:18 -0600

    Added foue different tests.
    Added these five tests for payBill():
    1. payLessThanTotal
    2. payFullWithChange (not working)
    3. payFullNoChange (not working)
    4. payTwice

--- com.autovend.software.test/src/com/autovend/software/test/PaymentWithCashTest.java
+++ com.autovend.software.test/src/com/autovend/software/test/PaymentWithCashTest.java
@@ -153,7 +153,7 @@ public class PaymentWithCashTest {
 		attendant = new MyAttendantIO();
 		
 		/* Load one hundred, $5, $10, $20, $50 bills into the dispensers so we can dispense change during tests.
-		 * Every dispenser has a max capacity of 100 bills. 
+		 * Every dispenser has a max capacity of 100 
 		 */
 		fiveDollarBills = new Bill[100];
 		tenDollarBills = new Bill[100];
@@ -207,8 +207,126 @@ public class PaymentWithCashTest {
 		}
 	}
 	
+	/* Test Case: An amount less than the cart total is paid. 
+	 * 
+	 * Description: The cart total is set at $100. $20 is paid in a single bill. 
+	 * 
+	 * There is no need to test for payments that would require coins, credit, or crypto.
+	 * 
+	 * Expected Result: The cart total should drop from $100 to $80. Checking the amount paid
+	 * should return a string value of "20".
+	 */
 	@Test
-	public void signals() {
+	public void payLessThanTotal() {
+		
+		paymentController.setCartTotal(100.00);
+		try {
+			selfCheckoutStation.billInput.accept(billTwenty);
+		} catch (DisabledException e) {
+			fail("A Disabled Exception should not have been thrown");
+		} catch (OverloadException e) {
+			fail("An OverloadException should not have been thrown");
+		}
+		assertEquals(80.00,paymentController.getCartTotal(),0.00);
+		assertEquals("20",paymentController.getAmountPaid());
+	}
+	
+	/* Test Case: The customer pays with a single bill on two separate instances. 
+	 * 
+	 * Description: The cart total is set at $100. $20 is paid in a single bill and
+	 * then $5 dollars is paid in a single bill. 
+	 * 
+	 * The purpose of this test is to see if any weird behaviors/occurrences happen to 
+	 * either the cart total or the amount paid.
+	 *    
+	 * There is no need to test for payments that would require coins, credit, or crypto.
+	 * 
+	 * Expected Result: The cart total should drop from $100 to $80. Then it should drop 
+	 * to $75 on the second bill insertion. Checking the amount paid should return a string
+	 * value of "20", then after the second bill insertion update to a string value of "25".
+	 */
+	@Test
+	public void payTwice() {
+		
+		paymentController.setCartTotal(100.00);
+		try {
+			// The customer first inserts a twenty dollar bill into the bill slot.
+			selfCheckoutStation.billInput.accept(billTwenty);
+			assertEquals(80.00,paymentController.getCartTotal(),0.00);
+			assertEquals("20",paymentController.getAmountPaid());
+			
+			// The customer then inserts a five dollar bill into the bill slot.
+			selfCheckoutStation.billInput.accept(billFive);
+			assertEquals(75.00,paymentController.getCartTotal(),0.00);
+			assertEquals("25",paymentController.getAmountPaid());
+			
+		} catch (DisabledException e) {
+			fail("A Disabled Exception should not have been thrown");
+		} catch (OverloadException e) {
+			fail("An OverloadException should not have been thrown");
+		}
+	}
+	
+	/* Test Case: The customer pays exactly the total cart amount. 
+	 * 
+	 * Description: The cart total is set at $50. $50 is paid in a single bill.
+	 * 
+	 * There shouldn't be a need to test this with multiple instances of paying with cash
+	 * as the previous test had covered any weird behaviors that could have occurred.
+	 *    
+	 * There is no need to test for payments that would require coins, credit, or crypto.
+	 * 
+	 * Expected Result: The cart total should drop from $50 to $0. 
+	 * Checking the amount paid should return a string value of "50".
+	 * Checking the total change should return a string value of be "0.00'.
+	 */
+	@Test
+	public void payFullNoChange() {
+		
+		paymentController.setCartTotal(50.00);
+		try {
+			// The customer pays the full fifty dollars using a single fifty dollar bill.
+			selfCheckoutStation.billInput.accept(billFifty);
+			assertEquals(0.00,paymentController.getCartTotal(),0.00);
+			assertEquals("50",paymentController.getAmountPaid());
+			assertEquals("0.00",paymentController.getTotalChange());
+			
+		} catch (DisabledException e) {
+			fail("A Disabled Exception should not have been thrown");
+		} catch (OverloadException e) {
+			fail("An OverloadException should not have been thrown");
+		}
+	}
+	
+	/* Test Case: The customer pays over the total cart amount. 
+	 * 
+	 * Description: The cart total is set at $10. $50 is paid in a single bill.
+	 * 
+	 * There shouldn't be a need to test this with multiple instances of paying with cash
+	 * as the previous test had covered any weird behaviors that could have occurred.
+	 *    
+	 * There is no need to test for payments that would require coins, credit, or crypto.
+	 * 
+	 * Expected Result: The cart total should drop from $10 to $0. 
+	 * Checking the amount paid should return a string value of "50".
+	 * Checking the total change should return a string value of be "40.00".
+	 */
+	@Test
+	public void payFullWithChange() {
+		
+		paymentController.setCartTotal(10.00);
+		try {
+			// The customer pays the full fifty dollars using a single fifty dollar bill.
+			selfCheckoutStation.billInput.accept(billFifty);
+			assertEquals(0.00,paymentController.getCartTotal(),0.00);
+			assertEquals("50",paymentController.getAmountPaid());
+			assertEquals("40.00",paymentController.getTotalChange());
+			
+		} catch (DisabledException e) {
+			fail("A Disabled Exception should not have been thrown");
+		} catch (OverloadException e) {
+			fail("An OverloadException should not have been thrown");
+		}
 	}
 
 	
